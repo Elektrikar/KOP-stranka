6e64585a020d217db3c51b1327b50e126becad56
Samuel Hangya <sami.hangya@gmail.com>
2026-01-16 17:32:49 +0100

    Fix description not displaying on related products

--- class/ProductCard.php
+++ class/ProductCard.php
@@ -4,13 +4,13 @@ class ProductCard {
     private $product;
     private $cartQuantity;
     private $context;
-    
+
     public function __construct($product, $cartQuantity = 0, $context = 'index') {
         $this->product = $product;
         $this->cartQuantity = $cartQuantity;
         $this->context = $context;
     }
-    
+
     public function render() {
         $product = $this->product;
         $cartQty = $this->cartQuantity;
@@ -22,15 +22,13 @@ class ProductCard {
 
         $html = '<div class="product-card" data-product-id="' . htmlspecialchars($product->id) . '">';
         $html .= '<div class="top">';
-
         $html .= '<a href="product_detail.php?id=' . $product->id . '" class="product-image-link">';
         $html .= '<img src="img/product/' . htmlspecialchars($product->image) . '" alt="' . htmlspecialchars($product->name) . '" class="product-image">';
         $html .= '</a>';
-
         $html .= '<div class="product-info">';
         $html .= '<h3 class="product-title"><a href="product_detail.php?id=' . $product->id . '">' . htmlspecialchars($product->name) . '</a></h3>';
 
-        if ($context === 'index') {
+        if ($context === 'index' || $context === 'related') {
             $html .= '<p class="product-description product-description-ellipsis">' . htmlspecialchars($product->description) . '</p>';
         } else if ($context === 'detail') {
             $html .= '<div class="product-description-full">' . nl2br(htmlspecialchars($product->description)) . '</div>';
@@ -39,7 +37,7 @@ class ProductCard {
         $html .= '<div class="product-stock-container">';
         $html .= '<div class="product-stock ' . $stockClass . '">' . $stockText . '</div>';
         $html .= '</div>';
-        
+
         $html .= '</div>';
         $html .= '</div>';
 
@@ -47,18 +45,19 @@ class ProductCard {
         $html .= '<div class="product-price">' . number_format($product->price, 2, ',', ' ') . ' €</div>';
 
         if ($product->stock <= 0) {
+            // No button if out of stock
         } else if ($inCart) {
             $html .= $this->renderCartSummary($cartQty);
         } else {
             $html .= '<button class="btn btn-cart add-to-cart" data-product-id="' . htmlspecialchars($product->id) . '">Do košíka</button>';
         }
-        
+
         $html .= '</div>';
         $html .= '</div>';
-        
+
         return $html;
     }
-    
+
     private function getStockStatus($stock) {
         if ($stock <= 0) {
             return ['out-of-stock', 'Momentálne nedostupné'];
@@ -68,7 +67,7 @@ class ProductCard {
             return ['in-stock', 'Skladom ' . $stock . ' ks'];
         }
     }
-    
+
     private function renderCartSummary($qty) {
         return '<div class="cart-summary">
             <div class="cart-label">V košíku</div>
@@ -95,12 +94,11 @@ class ProductCard {
         if (empty($products)) {
             return '';
         }
-        
-        // Limit to specified number (default 3)
+
         $products = array_slice($products, 0, $limit);
 
         $html = '<div class="products">';
-        
+
         foreach ($products as $product) {
             $cartQty = $cart->getQuantity($product['id']);
             $productObj = new stdClass();
@@ -110,7 +108,7 @@ class ProductCard {
             $card = new self($productObj, $cartQty, 'related');
             $html .= $card->render();
         }
-        
+
         $html .= '</div>';
         return $html;
     }
