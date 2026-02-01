1f131fbfd71843cfd9cff92d25ddc56580aabd7c
Samuel Hangya <sami.hangya@gmail.com>
2026-02-01 16:14:48 +0100

    Change simple formatting in categories

--- adminCategories.php
+++ adminCategories.php
@@ -95,7 +95,11 @@ require_once 'theme/header.php';
                     <?php endif; ?>
                 </td>
                 <td style="max-width: 200px;"><?= htmlspecialchars($cat['name']) ?></td>
-                <td style="max-width: 300px;"><?= htmlspecialchars(substr($cat['description'] ?? '', 0, 100)) ?></td>
+                <td style="max-width: 300px;"><?php 
+                    $desc = $cat['description'] ?? '';
+                    $display = strlen($desc) > 100 ? substr($desc, 0, 100) . '...' : $desc;
+                    echo htmlspecialchars($display);
+                ?></td>
                 <td><?= date('d.m.Y H:i', strtotime($cat['created_at'])) ?></td>
                 <td class="actions">
                     <a href="adminEdit.php?id=<?= $cat['id'] ?>&type=category" class="btn-edit"><svg width="20px" height="20px" viewBox="0 0 1024 1024"><path d="m199.04 672.64 193.984 112 224-387.968-193.92-112-224 388.032zm-23.872 60.16 32.896 148.288 144.896-45.696L175.168 732.8zM455.04 229.248l193.92 112 56.704-98.112-193.984-112-56.64 98.112zM104.32 708.8l384-665.024 304.768 175.936L409.152 884.8h.064l-248.448 78.336L104.32 708.8zm384 254.272v-64h448v64h-448z"/></svg></a>
